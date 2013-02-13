1e2bcb8a6dedc90ca9e3951e5ff20b3fe0bf10dc
Alexis Hassler <alexis@sewatech.org>
2013-02-13 17:11:42 +0100

    date

---
+++ JavaSource/fr/sewatech/formation/jsf/DateBean.java
@@ -0,0 +1,26 @@
+package fr.sewatech.formation.jsf;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean @ViewScoped
+public class DateBean implements Serializable{
+	private Date date;
+	
+	public DateBean() {
+		date = new Date();
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	
+}
